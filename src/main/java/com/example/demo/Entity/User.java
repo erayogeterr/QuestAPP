e14af3ffcc50ea387f02d13a22362e609879df75
Eray Ögeter <erayogeterr@gmail.com>
2022-10-10 14:57:56 +0300

    First Commit

---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	Long id;
+	
+	String userName;
+	String password;
+	
+
+}
